68d2724ad43e5c258c58cfe48401ae116aa0aef8
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2021-04-14 22:44:19 +0530

    Updates

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const hamburger = document.querySelector('.hamburger');
-hamburger.addEventListener('click', () => {
+hamburger.addEventListener('click', () => AnimateElements());
+document
+    .querySelector('main')
+    .addEventListener('click', () => AnimateElements());
+
+function AnimateElements() {
     document.querySelector('nav').classList.toggle('active');
     document.querySelectorAll('nav a').forEach((element, index) => {
         if (element.style.animation) {
             element.style.animation = '';
         } else {
-            element.style.animation = `linkAnimate ${0.7}s ease forwards ${
+            element.style.animation = `linkAnimate ${0.7}s linear forwards ${
                 index / 7 + 0.3
             }s`;
         }
     });
+    document.querySelector('.home-btn').classList.toggle('a-active');
     hamburger.classList.toggle('animate');
-});
+    document.body.classList.toggle('body-active');
+}
