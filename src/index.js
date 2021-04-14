10d9de015ddbf02530ecf3a9201d3dfd3b56403e
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2021-04-14 14:46:31 +0530

    last

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import reactDom from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './Components/App';
 import '../src/Components/sass/index.scss';
+// import './Components/index.css';
 
 reactDom.render(
     <BrowserRouter>
